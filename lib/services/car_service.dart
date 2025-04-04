9a889912da6c9abb73244d1558683d524b5e95f8
ehcarey <144972660+ehcarey@users.noreply.github.com>
2025-04-03 23:48:12 -0500

    Revert "Update car_service.dart"
    This reverts commit 274262c0bc59b1bef42be295430ced6944ed0a8f.

--- lib/services/car_service.dart
+++ lib/services/car_service.dart
@@ -13,41 +13,10 @@ class CarService {
       headers: {'Authentication': token},
     );
 
-   if (response.statusCode == 200) {
-      try {
-        List<dynamic> carsJson = jsonDecode(response.body);
-        _cars = carsJson.map((json) => Car.fromJson(json)).toList();
-        return _cars;
-      } catch (e) {
-        print('Error al convertir respuesta: $e');
-      }
+    if (response.statusCode == 200) {
+      List<dynamic> carsJson = jsonDecode(response.body);
+      return carsJson.map((json) => Car.fromJson(json)).toList();
     }
-
-    // Datos ficticios si la API falla
-    _cars = [
-      Car(id: 'ABC123', modelo: 'Model S', marca: 'a'),
-      Car(id: 'XYZ789', modelo: 'PICANT', marca: 'Ford'),
-      Car(id: 'LMN456', modelo: 'iX3', marca: 'BMW'),
-    ];
-    return _cars;
-  }
-
-  // Método para agregar un nuevo vehículo y actualizar la lista
-  Future<void> addCar(String token, Car newCar) async {
-    final response = await http.post(
-      Uri.parse('$_baseUrl/carros'),
-      headers: {
-        'Authentication': token,
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(newCar.toJson()),
-    );
-
-    if (response.statusCode == 201) {
-      _cars.add(newCar); // Agregar localmente para actualizar la UI
-    } else {
-      print('Error al agregar vehículo: ${response.body}');
-    }
+    return [];
   }
 }
-
